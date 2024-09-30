614e56319aeac60730670cf23a704bd81a018b3b
Max Eriksson <teratoscincus@hotmail.com>
2024-09-30 10:39:22 +0200

    feat(twilight): toggle on boot only

--- lua/plugins/twilight.lua
+++ lua/plugins/twilight.lua
@@ -3,19 +3,20 @@ local map = vim.keymap.set
 
 local exclude_ft = { -- Exclude these filetypes
   "gitcommit",
+  "notg",
 }
 
 return {
   "folke/twilight.nvim",
   opts = {
     dimming = {
-      alpha = 0.25,                -- Amount of dimming
+      alpha = 0.25, -- Amount of dimming
       color = { "Normal", colors.foreground },
       term_bg = colors.background, -- If guibg=NONE, this will be used to calculate text color
-      inactive = false,            -- When true, other windows will be fully dimmed (unless they contain the same buffer)
+      inactive = false, -- When true, other windows will be fully dimmed (unless they contain the same buffer)
     },
-    context = 10,                  -- Amount of lines we will try to show around the current line
-    treesitter = true,             -- Use treesitter when available for the filetype
+    context = 10, -- Amount of lines we will try to show around the current line
+    treesitter = true, -- Use treesitter when available for the filetype
     -- Treesitter is used to automatically expand the visible text,
     -- But you can further control the types of nodes that should always be fully expanded
     expand = { -- For treesitter, we we always try to expand to the top-most ancestor with these types
@@ -31,7 +32,8 @@ return {
 
     -- Toggle Twilight by default
     if require("config").twilight_auto_toggle then
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+      vim.api.nvim_create_autocmd({ "VimEnter" }, { -- NOTE: Toggles only on startup
+        -- vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, { -- TODO: Enable using setting, toggle on buff change
         pattern = "*",
         group = vim.api.nvim_create_augroup("auto_toggle_twilight", { clear = true }),
         callback = function()
