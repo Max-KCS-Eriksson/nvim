71b0921f573c6d91d01e678e53ac882b2a747f7d
Max Eriksson <teratoscincus@hotmail.com>
2025-04-04 11:37:12 +0200

    chore(plugins): fix config after update

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,45 +1,18 @@
-local map = vim.keymap.set
-
 return {
   {
     "folke/trouble.nvim",
     opts = {
       auto_preview = false,
+      focus = true,
     },
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-      -- Keymaps
-      map("n", "<leader>!!", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true, desc = "Trouble" })
-      map(
-        "n",
-        "<leader>!w",
-        "<cmd>TroubleToggle workspace_diagnostics<cr>",
-        { silent = true, noremap = true, desc = "Trouble workspace" }
-      )
-      map(
-        "n",
-        "<leader>!d",
-        "<cmd>TroubleToggle document_diagnostics<cr>",
-        { silent = true, noremap = true, desc = "Trouble document" }
-      )
-      map(
-        "n",
-        "<leader>!l",
-        "<cmd>TroubleToggle loclist<cr>",
-        { silent = true, noremap = true, desc = "Trouble local" }
-      )
-      map(
-        "n",
-        "<leader>!q",
-        "<cmd>TroubleToggle quickfix<cr>",
-        { silent = true, noremap = true, desc = "Trouble quickfix" }
-      )
-      map(
-        "n",
-        "<leader>!r",
-        "<cmd>TroubleToggle lsp_references<cr>",
-        { silent = true, noremap = true, desc = "Trouble LSP references" }
-      )
-    end,
+    cmd = { "Trouble" },
+    keys = {
+      { "<leader>!!", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>!b", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>!s", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>!L", "<cmd>Trouble lsp toggle<cr>", desc = "LSP Definitions / references / ... (Trouble)" },
+      { "<leader>!l", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>!q", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    },
   },
 }
