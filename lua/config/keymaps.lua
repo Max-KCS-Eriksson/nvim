65b87c20101337275e73b286af8620076c865e0a
Max Eriksson <teratoscincus@hotmail.com>
2025-08-23 12:14:21 +0200

    chore(keys): clarify description of folding keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,7 +18,7 @@ map("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
 map("n", "<leader>ss", "<C-w>v", { desc = "Split side" })
 map("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
 
-map("n", "zfs", "<cmd>normal ^[izf]i+<cr>", { desc = "Fold scope to bottom" })
+map("n", "zfs", "<cmd>normal ^[izf]i+<cr>", { desc = "Fold scope" })
 map("n", "zfm", "<cmd>normal ^[izf]i+]mzt<cr>", { desc = "Fold method" })
 
 map("n", "<leader>sw", "<cmd>normal yiwwviwpbbviwp<cr>", { desc = "Swap words" })
