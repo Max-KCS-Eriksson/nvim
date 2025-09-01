5bf37e8a37167a47c8e3ad33c20b429b3a433a6d
Max Eriksson <teratoscincus@hotmail.com>
2025-09-01 17:27:26 +0200

    feat(snippets): HTML snippets in `.jsx` files

--- lua/plugins/lsp/snippets.lua
+++ lua/plugins/lsp/snippets.lua
@@ -1,15 +1,24 @@
+local luasnip = require("luasnip")
+
 return {
   {
     "L3MON4D3/LuaSnip",
     dependencies = { "rafamadriz/friendly-snippets" },
     config = function()
-      local luasnip = require("luasnip")
-
       luasnip.config.setup({})
       require("luasnip.loaders.from_vscode").lazy_load()
 
       local snippets_dir = vim.fn.stdpath("config") .. "/snippets"
       require("luasnip.loaders.from_lua").load({ paths = { snippets_dir } })
+    end,
+  },
+  {
+    "rafamadriz/friendly-snippets",
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      -- HTML snippets for ReactJS
+      luasnip.filetype_extend("javascriptreact", { "html" })
     end,
   },
 }
